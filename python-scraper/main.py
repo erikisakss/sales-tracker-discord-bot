fbe374b78bbe3b2f07390ee6fbc6c8781e33a007
erikisakss <erik@isaksson.biz>
2024-04-14 23:13:07 +0200

    Update main.py

--- python-scraper/main.py
+++ python-scraper/main.py
@@ -8,4 +8,7 @@ app = Flask(__name__)
 def apicall(name):
     url = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/'
     response = requests.get(url)
-    return response.json()
+    data = response.json()
+
+    matching_apps = [app for app in data['applist']['apps'] if name.lower() in app['name'].lower()]
+    return {'matching_apps': matching_apps}
