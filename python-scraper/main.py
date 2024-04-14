8e1bdfe33a20e1504dcce48298e78b72c54e8e84
erikisakss <erik@isaksson.biz>
2024-04-14 23:04:34 +0200

    Added requests

--- python-scraper/main.py
+++ python-scraper/main.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route('/<name>')
 
-def hello_world(name):
-    return 'Hello, {}!'.format(name)
+def apicall(name):
+    url = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/'
+    response = requests.get(url)
+    return response.json()
